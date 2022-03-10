4ca7a8ae5eafbb937c95ec27166246faf4bdf625
Jiří Valášek <jirka362@gmail.com>
2022-03-10 03:23:06 +0100

    Additional setup
    - Added docs redirect
    - python types testing
    - improved vscode settings

--- dacb4ea/algorithms/AbstractEvolutionAlg.py
+++ dacb4ea/algorithms/AbstractEvolutionAlg.py
@@ -1,10 +1,11 @@
 from abc import ABC
+from typing import Dict
 
 
 class AbstractEvolutionAlg(ABC):
     """An abstract class to be inherited by evolutionary algorithms such as
     Differential Evolution, Swarm Particle Optimization etc."""
 
-    def __init__(self, init_params):
+    def __init__(self, init_params: Dict[str, int]) -> None:
 
         pass
